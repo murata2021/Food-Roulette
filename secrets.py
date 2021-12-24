ffb68c15119fcb12f224572e6fae24d586b39776
Murat Arslan <80642557+murata2021@users.noreply.github.com>
2021-12-24 16:00:31 -0500

    Delete secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-####Keep them in seperate file and add that file to .gitignore
-
-
-api_key='VXDgcWXwlWYhFE7iK2_EFSYlPrw_FfRM9yTRjdTOmJ1RrzUyXubnUzsSMM7x1bw8yualF5eIjQmh2rMJPfnP5PUnZoy4Aer4hPoCG0rOjcEyY7urCGXaBgbkTjelYXYx'
-
